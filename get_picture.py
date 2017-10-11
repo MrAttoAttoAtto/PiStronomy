65bcf7e714e6cea148794e3499ccb0c9926d5f69
MrAttoAttoAtto <atto.allas@gmail.com>
2017-10-11 21:57:10 +0100

    Changed to requests

--- get_picture.py
+++ get_picture.py
@@ -1,4 +1,10 @@
-from urllib.requests import urlretrieve
+import requests
+from PIL import Image
+from io import BytesIO
 
-def get_picture(url):
-    urlretrieve(url, 'cool_beans.png')
+def get_sky_picture(paramDict=None):
+    httpResponse = requests.get('http://server4.wikisky.org/map', params=paramDict)
+
+    image = Image.open(BytesIO(httpResponse.content))
+
+    image.show()
