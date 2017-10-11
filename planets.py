034b5029e7aba2b3d45e556de07f4b2eec7783e6
MrAttoAttoAtto <atto.allas@gmail.com>
2017-10-11 23:53:15 +0100

    Caps!

--- planets.py
+++ planets.py
@@ -7,7 +7,7 @@ def get_planet_coords():
     formattedTime = time.strftime("%Y-%m-%d %H:%M")
     realTime = Time(formattedTime)
 
-    RealLocation = EarthLocation.of_site('greenwich')
+    realLocation = EarthLocation.of_site('greenwich')
 
     '''
     0 - Merc
@@ -22,16 +22,16 @@ def get_planet_coords():
     skyCoordList = []
 
     with solar_system_ephemeris.set('de432s'):
-        skyCoordList.append(get_body('mercury', realTime, RealLocation))
-        skyCoordList.append(get_body('venus', realTime, RealLocation))
-        skyCoordList.append(get_body('mars', realTime, RealLocation))
-        skyCoordList.append(get_body('jupiter', realTime, RealLocation))
-        skyCoordList.append(get_body('saturn', realTime, RealLocation))
-        skyCoordList.append(get_body('uranus', realTime, RealLocation))
-        skyCoordList.append(get_body('neptune', realTime, RealLocation))
-        skyCoordList.append(get_body('pluto', realTime, RealLocation))
-        skyCoordList.append(get_body('sun', realTime, RealLocation))
-        skyCoordList.append(get_body('moon', realTime, RealLocation))
+        skyCoordList.append(get_body('mercury', realTime, realLocation))
+        skyCoordList.append(get_body('venus', realTime, realLocation))
+        skyCoordList.append(get_body('mars', realTime, realLocation))
+        skyCoordList.append(get_body('jupiter', realTime, realLocation))
+        skyCoordList.append(get_body('saturn', realTime, realLocation))
+        skyCoordList.append(get_body('uranus', realTime, realLocation))
+        skyCoordList.append(get_body('neptune', realTime, realLocation))
+        skyCoordList.append(get_body('pluto', realTime, realLocation))
+        skyCoordList.append(get_body('sun', realTime, realLocation))
+        skyCoordList.append(get_body('moon', realTime, realLocation))
 
     coordinates = [(skyCoord.ra.hour, skyCoord.dec.degree) for skyCoord in skyCoordList]
 
