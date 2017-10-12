ad2b53623cfeee0a6ec85be23eb88915aa357a9e
MrAttoAttoAtto <atto.allas@gmail.com>
2017-10-12 22:36:27 +0100

    good stuff

--- get_picture.py
+++ get_picture.py
@@ -2,8 +2,14 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-def get_sky_picture(paramDict=None):
+def get_sky_picture(paramDict=None, ra=None, de=None):
     paramDict['survey'] = 'DSS2'
+
+    if ra != None:
+        paramDict['ra'] = ra
+    
+    if de != None:
+        paramDict['de'] = de
 
     httpResponse = requests.get('http://server4.wikisky.org/imgcut', params=paramDict)
 
