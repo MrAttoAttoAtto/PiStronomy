666c59ebe52b5ccd4167e28d9f057907e663272a
MrAttoAttoAtto <atto.allas@gmail.com>
2017-11-28 21:18:57 +0000

    Major leaps and bounds

--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 '''Tools for converting between units (and possibly other things)'''
+import urllib.parse
 
 def from_hour_rep(hours, mins, secs):
     answer = hours % 24
@@ -10,10 +11,19 @@ def from_hour_rep(hours, mins, secs):
     return answer
 
 def from_min_rep(deg, mins, secs):
+    if abs(deg) != deg:
+        negative = True
+    else:
+        negative = False
+
     answer = deg
 
     answer += mins/60
 
     answer += secs/3600
 
-    return answer
+    return -answer if negative else answer
+
+def from_hex_unicode_rep(ssid):
+    ssid = ssid.encode('latin-1').decode('utf8')
+    return urllib.parse.unquote(ssid)
